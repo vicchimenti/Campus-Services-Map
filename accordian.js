b184bf5a5edc670150d55a41e39bf5925bd8088a
Vic Chimenti <vicchimenti@users.noreply.github.com>
2018-11-02 13:40:44 -0700

    Update accordian.js

--- accordian.js
+++ accordian.js
@@ -1,1 +1,29 @@
-<!-- accordian.js -->
+// ***  accordian.js *** //
+
+
+/// *** initialize the accordian
+function initializeAccordian(accordianElement) {
+
+  // *** change panel from open or close *** //
+  function handlePanelClick(event) {
+    showPanel(event.CurrentTarget);
+  }
+
+  // *** open new panel and close last one *** //
+  funtion showPanel(panel) {
+    var openPanel = accordianElement.querySelctor(".active");
+    if (openPanel) {
+      openPanel.classList.remove("active");
+    }
+
+    // *** open new panel *** //
+    panel.classList.add("active");
+  }
+
+  var allPanelElements = accordianElement.querySelctorAll(".panel");
+  for (var i = 0, len = allPanelElements.length; i < len; i++) {
+    allPanelElements[i].addEventListener("click", handlePanelClick);
+  }
+}
+
+initializeAccordian(document.getElementById("accordian"));
