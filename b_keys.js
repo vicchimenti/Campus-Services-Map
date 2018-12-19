6c59282b91abf0c4b94ad19608fb3cfd5631769a
Vic Chimenti <vicchimenti@users.noreply.github.com>
2018-12-19 11:29:06 -0800

    Delete b_keys.js

--- b_keys.js
+++
@@ -1,4 +0,0 @@
-// *** campus_resource_map.js *** //
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAl51bxzHfJlGn9--0VhBtEMpDHknYu6sI&callback=initialize" async defer></script>
-<t4 type="media" formatter="text/javascript" id="367616" />
